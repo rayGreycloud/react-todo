3721ccde16fa08508f429240d5c4a0896bd41a45
rayGreycloud <rayGreycloud@gmail.com>
2016-06-15 16:26:49 -0700

    Complete handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -10,26 +11,34 @@ var TodoApp = React.createClass({
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Walk the Dog'
         },
         {
-          id: 2,
+          id: uuid(),
           text: 'Clean the kitchen'
         },
         {
-          id: 3,
+          id: uuid(),
           text: 'Wash the car'
         },
         {
-          id: 4,
+          id: uuid(),
           text: 'Fold laundry'
         }
       ]
     };
   },
   handleAddTodo: function (text) {
-    alert('new todo: ' + text);
+    this.setState({
+      todos: [
+      ...this.state.todos,
+      {
+        id: uuid(),
+        text: text
+      }
+      ]
+    });
   },
   handleSearch: function (showCompleted, searchText) {
     this.setState({
