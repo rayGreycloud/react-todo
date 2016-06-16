c06cb4f75493e55813fd3d2c1d3e56444d9631e1
rayGreycloud <rayGreycloud@gmail.com>
2016-06-16 16:42:12 -0700

    Add completed and created timestamps

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var uuid = require('node-uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -21,11 +22,13 @@ var TodoApp = React.createClass({
     this.setState({
       todos: [
       ...this.state.todos,
-      {
-        id: uuid(),
-        text: text,
-        completed: false
-      }
+        {
+          id: uuid(),
+          text: text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
       ]
     });
   },
@@ -33,6 +36,7 @@ var TodoApp = React.createClass({
     var updatedTodos = this.state.todos.map((todo) => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
 
       return todo;
