a302007ed3464c487eba7ceacd017262d4234e8a
rayGreycloud <rayGreycloud@gmail.com>
2016-06-21 17:29:13 -0700

    Refactor Todo, TodoApp, and TodoList and tests for redux

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,14 +1,14 @@
 var React = require('react');
-var Todo = require('Todo');
 var {connect} = require('react-redux');
+import Todo from 'Todo';
 
-var TodoList = React.createClass({
+export var TodoList = React.createClass({
   render: function () {
     var {todos} = this.props;
     var renderTodos = () => {
       if (todos.length === 0) {
         return (
-          <p className="container__message">Nothing to Do</p>
+          <p className="container__message">Nothing To Do</p>
         );
       }
       return todos.map((todo) => {
@@ -26,7 +26,7 @@ var TodoList = React.createClass({
   }
 });
 
-module.exports = connect(
+export default connect(
   (state) => {
     return {
       todos: state.todos
