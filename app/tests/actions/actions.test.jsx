25dff80bacef10abf9179fd148c9b97c5b61bd6f
rayGreycloud <rayGreycloud@gmail.com>
2016-06-22 10:44:39 -0700

    Refactor app to work w/local storage

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -31,6 +31,23 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate ADD_TODOS action object', () => {
+    var todos = [{
+      id: '111',
+      text: 'Anything',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 33000
+    }];
+    var action = {
+      type: 'ADD_TODOS',
+      todos
+    };
+    var res = actions.addTodos(todos);
+
+    expect(res).toEqual(action);
+  });
+
   it('should generate toggle todo action', () => {
     var action = {
       type: 'TOGGLE_TODO',
