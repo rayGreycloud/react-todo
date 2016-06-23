967a700a7e379b09c72f9c389bf13bd6251973ac
rayGreycloud <rayGreycloud@gmail.com>
2016-06-23 14:04:52 -0700

    Refactor to fetch todos from firebase

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,24 +1,6 @@
 var $ = require('jquery');
 
 module.exports = {
-  setTodos: function (todos) {
-    if ($.isArray(todos)) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-      return todos;
-    }
-  },
-  getTodos: function () {
-    var stringTodos = localStorage.getItem('todos');
-    var todos = [];
-
-    try {
-      todos = JSON.parse(stringTodos);
-    } catch (e) {
-
-    }
-
-    return $.isArray(todos) ? todos : [];
-  },
   filterTodos: function (todos, showCompleted, searchText) {
     var filteredTodos = todos;
 
@@ -28,7 +10,7 @@ module.exports = {
 
     filteredTodos = filteredTodos.filter((todo) => {
       var todoText = todo.text.toLowerCase();
-      return searchText.length === 0 || todoText.indexOf(searchText) > -1;  
+      return searchText.length === 0 || todoText.indexOf(searchText) > -1;
 
     });
 
