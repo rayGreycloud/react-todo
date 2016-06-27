f5771ab2d98f9f6879f4dbbcc178db25f4957ac2
rayGreycloud <rayGreycloud@gmail.com>
2016-06-27 16:27:43 -0700

    Add Login route and component

---
+++ app/components/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export var Login = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1 className="page-title">Todo App</h1>
+
+        <div className="row">
+          <div className="columns small-centered small-10 medium-6 large-4">
+            <div className="callout callout-auth">
+              <h3>Login</h3>
+              <p>
+                Login with GitHub account below
+              </p>
+              <button className="button">Login with GitHub</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+export default Login;
